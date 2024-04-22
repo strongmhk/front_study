e5a379705eab5385cec12f77579f49f07968b654
strongmhk <kim6562166086@gmail.com>
2024-04-22 21:43:22 +0900

    feat: state 사용

--- react/atoz/react-todo-app/src/App.js
+++ react/atoz/react-todo-app/src/App.js
@@ -2,6 +2,22 @@ import React, {Component} from "react";
 import "./App.css";
 
 export default class App extends Component {
+
+  state= {
+    todoData : [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true,
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false,
+      },
+    ]
+  };
+
   btnStyle = {
     color: "#fff",
     border: "none",
@@ -18,26 +34,9 @@ export default class App extends Component {
     };
   };
 
-  todoData = [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true,
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false,
-    },
-    {
-      id: "3",
-      title: "운동하기",
-      completed: false,
-    },
-  ];
-
   handleClick = (id) => {
-    let newTodoData = this.todoData.filter((data) => data.id !== id);
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    this.setState({todoData: newTodoData })
     console.log('newTodoData', newTodoData);
   }
 
@@ -49,7 +48,7 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
           
-        {this.todoData.map((data) => (
+        {this.state.todoData.map((data) => (
           <div style={this.getStyle()} key={data.id} >
             <input type="checkbox" defaultChecked={false} />
               {data.title}
