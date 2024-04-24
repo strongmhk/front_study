2ece8e40d76efe122ac100136f31a61c5b94765a
strongmhk <kim6562166086@gmail.com>
2024-04-24 21:37:39 +0900

    feat: class -> 함수형 컴포넌트로 변경

--- react/atoz/react-todo-app/src/App.js
+++ react/atoz/react-todo-app/src/App.js
@@ -1,25 +1,24 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import "./App.css";
 
-export default class App extends Component {
+export default function App() {
 
-  state= {
-    todoData : [
-      {
-        id: "1",
-        title: "공부하기",
-        completed: false,
-      },
-      {
-        id: "2",
-        title: "청소하기",
-        completed: false,
-      },
-    ],
-    value: "",
-  };
+  const [todoData, setTodoData] = useState([
+    {
+      id: "1",
+      title: "공부하기",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+  ]);
+  
+  const [value, setValue] = useState("");
 
-  btnStyle = {
+  const btnStyle = {
     color: "#fff",
     border: "none",
     padding: "5px 9px",
@@ -27,7 +26,7 @@ export default class App extends Component {
     float: "right"
   };
 
-  getStyle = (completed) => {
+  const getStyle = (completed) => {
     return {
       padding : "10px",
       borderBottom: "1px #ccc dotted",
@@ -35,43 +34,44 @@ export default class App extends Component {
     };
   };
 
-  handleClick = (id) => {
-    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
-    this.setState({todoData: newTodoData })
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter((data) => data.id !== id);
     console.log('newTodoData', newTodoData);
+    setTodoData(newTodoData);
   }
 
-  handleChange = (e) => {
-    this.setState({value: e.target.value});
+  const handleChange = (e) => {
+    setValue(e.target.value);
   }
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     // form 안에 input을 전송할 때 페이지가 리로드 되는 것을 막아줌
     e.preventDefault();
 
     // 새로운 할 일 데이터
     let newTodo = {
       id: Date.now(),
-      title: this.state.value,
+      title: value,
       completed: false,
     };
 
     // 원래 있던 할 일에 새로운 할 일 더해주기
     // 입력란에 글씨 지워주기
-    this.setState({ todoData: [...this.state.todoData, newTodo], value: "" });
+    setTodoData((prev) => [...prev, newTodo]);
+    setValue("");
   }
 
-  handleCompleteChange = (id) => {
-    let newTodoData = this.state.todoData.map((data) => {
+  const handleCompleteChange = (id) => {
+    let newTodoData = todoData.map((data) => {
       if (data.id === id) {
         data.completed = !data.completed;
       }
       return data;
     });
-    this.setState({ todoData: newTodoData });
+    
+    setTodoData(newTodoData);
   };
 
-  render() {
     return(
       <div className="container">
         <div className="todoBlock">
@@ -79,26 +79,26 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
           
-        {this.state.todoData.map((data) => (
-          <div style={this.getStyle(data.completed)} key={data.id} >
+        {todoData.map((data) => (
+          <div style={getStyle(data.completed)} key={data.id} >
             <p>
               <input type="checkbox" 
-              onChange={() => this.handleCompleteChange(data.id)}
+              onChange={() => handleCompleteChange(data.id)}
               defaultChecked={false} />
                 {data.title}
-              <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
+              <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
             </p>
           </div>
         ))}  
 
-          <form style={{ display: "flex"}} onSubmit={this.handleSubmit}>
+          <form style={{ display: "flex"}} onSubmit={handleSubmit}>
             <input 
             type="text" 
             name="value" 
             style={{ flex: '10', padding: '5px'}}
             placeholder="해야 할 일을 입력하세요." 
-            value={this.state.value}
-            onChange={this.handleChange}
+            value={value}
+            onChange={handleChange}
             />
           <input 
             type="submit"
@@ -111,5 +111,5 @@ export default class App extends Component {
         </div>
       </div>
     )
-  }
+
 }
